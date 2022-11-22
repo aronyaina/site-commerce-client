23a3bdae619e4c88830fd0b1a0acaa93dfbdfc95
aronyaina <ravaronyaina@gmail.com>
2022-11-22 21:20:51 +0300

    Bootstrap

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,27 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 export default function navbar() {
-  return <div></div>;
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>m-shop</h1>
+        </Link>
+        <Link to="/">
+          <p>Acceuil</p>
+        </Link>
+        <Link to="/">
+          <p>Shop</p>
+        </Link>
+        <Link to="/">
+          <p>About</p>
+        </Link>
+        <Link to="/">
+          <h1>LOGIN</h1>
+        </Link>
+      </div>{" "}
+    </header>
+  );
 }
