3ea713958633957ea50510981b73926587e15a39
aronyaina <ravaronyaina@gmail.com>
2022-11-24 15:25:38 +0300

    before fix react

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,15 @@
-import React from "react";
-
 import { Link } from "react-router-dom";
 
-export default function navbar() {
+const Navbar = () => {
   return (
     <header>
       <div className="container">
         <Link to="/">
-          <h1>m-shop</h1>
+          <h1>M-SHOP</h1>
         </Link>
-        <Link to="/">
-          <p>Acceuil</p>
-        </Link>
-        <Link to="/">
-          <p>Shop</p>
-        </Link>
-        <Link to="/">
-          <p>About</p>
-        </Link>
-        <Link to="/">
-          <h1>LOGIN</h1>
-        </Link>
-      </div>{" "}
+      </div>
     </header>
   );
-}
+};
+
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
 const Home = () => {
+  const SERVER_URI = "/api/products";
+  const [products, setProduct] = useState(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const response = await fetch(SERVER_URI);
+      const json = await response.json();
+      console.log(json);
+
+      if (response.ok) {
+        setProduct(json);
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="products">
+        {products &&
+          products.product.map((product) => (
+            <p key={product._id}>{product.name}</p>
+          ))}
+      </div>
     </div>
   );
 };
