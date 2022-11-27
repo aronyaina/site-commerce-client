cd40372aec0789d2210674e2138e6473c1ec6982
aronyaina <ravaronyaina@gmail.com>
2022-11-27 15:49:49 +0300

    axios integration get request

--- src/components/productForm.js
+++ src/components/productForm.js
@@ -1,4 +1,5 @@
 import { React, useState } from "react";
+import axios from "axios";
 function ProductForm() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
@@ -14,49 +15,71 @@ function ProductForm() {
       price,
       quantity,
     };
-
-    const response = await fetch("/api/products", {
-      method: "post",
-      accept: "application/json",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      body: JSON.stringify({
-        name: "Souris",
-        description: "Bonne souris",
-        price: "3200",
-        quantity: "3",
-      }),
-    })
-      .then((response) => response.json())
-      .then((product) => {
+    const stringify = JSON.stringify(product);
+    console.log(typeof stringify);
+    console.log(stringify);
+    const url = "/api/products";
+    axios
+      .post(url, stringify, {
+        headers: {
+          "Content-Type": "application/json",
+          "content-type": "application/json;charset=utf-8",
+        },
+      })
+      .then(({ stringify }) => {
+        console.log(stringify);
         setError(null);
         setName("");
         setDescription("");
         setPrice("");
         setQuantity("");
-        console.log("Success:", product);
       })
       .catch((error) => {
         console.error("Error:", error);
+        setError(error);
       });
+    // const response = await fetch("/api/products", {
+    //   method: "post",
+    //   accept: "application/json",
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json;charset=UTF-8",
+    //   },
+    //   body: JSON.stringify({
+    //     name: "Souris",
+    //     description: "Bonne souris",
+    //     price: "3200",
+    //     quantity: "3",
+    //   }),
+    // })
+    //   .then((response) => response.json())
+    //   .then((product) => {
+    //     setError(null);
+    //     setName("");
+    //     setDescription("");
+    //     setPrice("");
+    //     setQuantity("");
+    //     console.log("Success:", product);
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error:", error);
+    //   });
 
-    // const json = await response.json();
+    // // const json = await response.json();
 
-    // if (!response.ok) {
-    //   setError(json.error);
-    //   console.log(error);
-    // }
+    // // if (!response.ok) {
+    // //   setError(json.error);
+    // //   console.log(error);
+    // // }
 
-    // if (response.ok) {
-    //   setError(null);
-    //   setName("");
-    //   setDescription("");
-    //   setPrice("");
-    //   setQuantity("");
-    //   console.log("Product added !", json);
-    // }
+    // // if (response.ok) {
+    // //   setError(null);
+    // //   setName("");
+    // //   setDescription("");
+    // //   setPrice("");
+    // //   setQuantity("");
+    // //   console.log("Product added !", json);
+    // // }
   };
 
   return (
